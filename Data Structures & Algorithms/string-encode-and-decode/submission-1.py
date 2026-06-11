f954a7c7df057682f6a8b1247e6acc536f4a9ab2
Tyrone Cheang <tyronecheang@gmail.com>
2026-06-11 12:16:53 -0700

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        parts = []
+        for s in strs:
+            parts.append(s)
+            parts.append("-")
+        encoded_string = "".join(parts)
+        return encoded_string
+
+    def decode(self, s: str) -> List[str]:
+        parts = s.split("-")
+        parts = parts[:-1]
+        decoded_strs = []
+        for part in parts:
+            decoded_strs.append(part)
+        return decoded_strs
+        
+
